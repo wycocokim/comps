ad96efa9f8713f189c5ddfee20fa2c6a6236e39a
Kim Kenet Wycoco <wycocokim@gmail.com>
2023-05-23 12:40:11 +0800

    2nd commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import DropdownPage from "./pages/DropdownPage";
 import ButtonPage from "./pages/ButtonPage";
 import ModalPage from "./pages/ModalPage";
 import TablePage from "./pages/TablePage";
+import CounterPage from "./pages/CounterPage";
 
 function App() {
   return (
@@ -22,6 +23,9 @@ function App() {
         </Route>
         <Route path="/modal">
           <ModalPage />
+        </Route>
+        <Route path="/counter">
+          <CounterPage initialCount={10} />
         </Route>
         <Route path="/table">
           <TablePage />
